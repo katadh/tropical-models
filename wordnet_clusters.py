d0c6c875aea76ae9df472859ab5c32243fd8ba4c
katadh <katadh@sbcglobal.net>
2017-03-12 19:38:51 -0400

    progress on using LDA on local corpus, getting topics, and visualizing wordnet clusters

--- wordnet_clusters.py
+++ wordnet_clusters.py
@@ -1,9 +1,31 @@
 from nltk.corpus import wordnet as wn
 
-def word_distances(words):
+import numpy as np
 
-    word_dists = {}
+from matplotlib import pyplt as plt
+
+from sklearn import manifold
+
+#def wordnet_distances(words):
+#
+#    word_dists = {}
+#
+#    for i in range(len(words)):
+#        for j in range(i+1, len(words)):
+#            word_dists[(words[i], words[j])] = wn.path_similarity(words[i], words[j])
+
+#We want an actual matrix in order to use sklearn MDS
+def wordnet_distances(words):
+    word_dists = np.zeros(len(words)**2, dtype='float64').reshape(len(words), len(words))
 
     for i in range(len(words)):
         for j in range(i+1, len(words)):
-            word_dists[(words[i], words[j])] = wn.path_similarity(words[i], words[j])
+            dist = wn.path_similarity(words[i], words[j])
+            word_dists[i, j] = dist
+            word_dists[j, i] = dist
+
+def visualize_distance_matrix(dists):
+
+    nmds = manifold.MDS(n_components=2, metric=False, max_iter=3000, eps=1e-12,
+                        dissimilarity="precomputed", random_state=666, n_jobs=-1)
+    points = nmds.fit(dists).embedding_
