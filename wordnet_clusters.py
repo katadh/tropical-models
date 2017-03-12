1509ff114cd6de233d0557201dbd7826ac170a87
katadh <katadh@sbcglobal.net>
2017-03-12 14:58:20 -0400

    initial function to get all word distances

--- wordnet_clusters.py
+++ wordnet_clusters.py
@@ -1,1 +1,9 @@
-import nltk
+from nltk.corpus import wordnet as wn
+
+def word_distances(words):
+
+    word_dists = {}
+
+    for i in range(len(words)):
+        for j in range(i+1, len(words)):
+            word_dists[(words[i], words[j])] = wn.path_similarity(words[i], words[j])
