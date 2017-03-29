8451c7223aabf682c73bab85356f55b71586f36f
katadh <katadh@sbcglobal.net>
2017-03-29 13:33:52 -0400

    changed getting documents from threads to processes, still requires testing

--- wiki_local.py
+++ wiki_local.py
@@ -5,9 +5,11 @@ import random
 import json
 import re
 
+from multiprocessing import Pool
+
 import wsd
 
-def get_random_wikipedia_article():
+def get_random_wikipedia_article(dummy_input):
     wiki_path = "../json_articles/"
     rand_folder = random.choice(os.listdir(wiki_path))
     rand_file = random.choice(os.listdir(wiki_path+rand_folder))
@@ -22,37 +24,51 @@ def get_random_wikipedia_article():
     #text = re.sub(r' +', ' ', text)
     #print text
     #print text
-    #text = wsd.WSD(text)
+    text = wsd.WSD(text)
     return (text, title)
 
-class WikiThread(threading.Thread):
-    articles = list()
-    articlenames = list()
-    lock = threading.Lock()
-
-    def run(self):
-        (article, articlename) = get_random_wikipedia_article()
-        WikiThread.lock.acquire()
-        try:
-            article = wsd.WSD(article)
-            WikiThread.articles.append(article)
-            WikiThread.articlenames.append(articlename)
-        except Exception as ex: #catch and print message for basically anything that's not a system error or keyboard interrupt
-            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-            message = template.format(type(ex).__name__, ex.args)
-            print message
-        WikiThread.lock.release()
+#class WikiThread(threading.Thread):
+#    articles = list()
+#    articlenames = list()
+#    lock = threading.Lock()
+#
+#    def run(self):
+#        (article, articlename) = get_random_wikipedia_article()
+#        WikiThread.lock.acquire()
+#        try:
+#            article = wsd.WSD(article)
+#            WikiThread.articles.append(article)
+#            WikiThread.articlenames.append(articlename)
+#        except Exception as ex: #catch and print message for basically anything that's not a system error or keyboard interrupt
+#            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+#            message = template.format(type(ex).__name__, ex.args)
+#            print message
+#        WikiThread.lock.release()
 
 def get_random_wikipedia_articles(n):
-    maxthreads = 8
-    WikiThread.articles = list()
-    WikiThread.articlenames = list()
-    wtlist = list()
-    for i in range(0, n, maxthreads):
-        print 'downloaded %d/%d articles...' % (i, n)
-        for j in range(i, min(i+maxthreads, n)):
-            wtlist.append(WikiThread())
-            wtlist[len(wtlist)-1].start()
-        for j in range(i, min(i+maxthreads, n)):
-            wtlist[j].join()
-    return (WikiThread.articles, WikiThread.articlenames)
+
+    print "getting wiki articles"
+    pool = Pool(processes=8)
+
+    dummy_inputs = [''] * n
+    article_title_pairs = pool.map(get_random_wikipedia_article, dummy_inputs)
+
+    articles = [pair[0] for pair in article_title_pairs]
+    titles = [pair[1] for pair in article_title_pairs]
+
+    print "finished getting wiki articles, timespent"
+
+    return (articles, titles)
+    
+    ##maxthreads = 8
+    ##WikiThread.articles = list()
+    ##WikiThread.articlenames = list()
+    ##wtlist = list()
+    ##for i in range(0, n, maxthreads):
+    ##    print 'downloaded %d/%d articles...' % (i, n)
+    ##    for j in range(i, min(i+maxthreads, n)):
+    ##        wtlist.append(WikiThread())
+    ##        wtlist[len(wtlist)-1].start()
+    ##    for j in range(i, min(i+maxthreads, n)):
+    ##        wtlist[j].join()
+    ##return (WikiThread.articles, WikiThread.articlenames)
