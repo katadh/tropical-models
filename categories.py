7367a965a34a1d1cd29a25a6520c4528cd4d32ed
Kristen Allen <kristen.c.allen@gmail.com>
2017-04-09 15:09:31 -0400

    tweak categories to only consider items in namespace 0

--- categories.py
+++ categories.py
@@ -7,15 +7,19 @@ fn = "/Users/kristen/Box Sync/Semantics/Wikipedia-20170407184627.xml"
 def scan_file(fn, cat_dict):
 	with open(fn) as f:
 		on_art_id = False
+		rightns = False
 		for line in f:
 			if line == "  <page>\n": #track whether we're on first ID in a new page
 				on_art_id = True
 				#print "saw a page tag; set on_art_id to True"
+			elif line.split('>')[0] == "    <ns":
+				if line.split('>')[1].split('<')[0] == "0":
+					rightns = True
 			elif on_art_id and line.split('>')[0] == '    <id':
 				curr_id = line.split('>')[1].split('<')[0]
 				on_art_id = False #revision IDs will start coming up; ignore those
 				#print "just set curr_id to", curr_id
-			elif line.split(':')[0] == "[[Category":
+			elif rightns == True and line.split(':')[0] == "[[Category":
 				cat=line.split(':')[1].split(']')[0]
 				if '|' in cat:
 					cat = cat.split('|')[0]
